294872f18734a63965a9644f70e44c3edcb98c47
JD Santos <28938316+jd-santos@users.noreply.github.com>
2025-10-27 14:34:17 -0500

    Add LazyVim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,21 +1,2 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- Check if lazy.nvim is installed; if not, clone it.
-if not vim.fn.isdirectory(lazypath) then
-  print("Cloning lazy.nvim...") 
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", 
-    lazypath,         
-  })
-end
-
--- Prepend lazy.nvim to the runtime path.
-vim.opt.rtp:prepend(lazypath)
-
-local plugins = {}
-local opts = {}
-
-require("lazy").setup(plugins,opt)
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
